2efc7639745a4fac69f4a9e15a9dfbbf82fa4218
Adhithyan-A-hub <adhithyan.alagudurai@gmail.com>
2025-07-17 00:55:37 +0530

    Add files via upload

---
+++ Looping statements/do_while_loop.js
@@ -0,0 +1,28 @@
+let n;
+
+function print_n(n){
+
+    do{
+        console.log(n);
+        n++;
+    }
+    while(n<=10);
+
+}
+
+print_n(1)
+
+console.log("loop for 10-1");
+
+
+function print_reverse(n){
+
+    do{
+        console.log(n);
+        n--;
+    }
+    while(n >=1);
+
+}
+
+print_reverse(10);
---
+++ conditional statement/if_else.js
@@ -0,0 +1,24 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function check() {
+  rl.question("/n Enter a number: ",
+    function (input) {
+      const number = Number(input); // convert string to number
+
+      if (number %5 === 0) {
+        console.log("You entered:", number, "is divisible by 5");
+        rl.close();
+    } else {
+        console.error("Number",number, "is not divisible by 5 !");
+        console.log("\n Wanna try again ?");
+        check();
+      }
+    });
+}
+
+check();
